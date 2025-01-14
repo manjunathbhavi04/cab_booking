3e2dc25634353fe7e685c89563802179f0febd22
manjunathbhavi04 <manjunathbhavi890@gmail.com>
2025-01-14 16:54:31 +0530

    addded booking and cab model

---
+++ src/main/java/com/example/bookYourCab/enums/TripStatus.java
@@ -0,0 +1,6 @@
+package com.example.bookYourCab.enums;
+
+public enum TripStatus {
+    Completed,
+    pending
+}
---
+++ src/main/java/com/example/bookYourCab/model/Booking.java
@@ -0,0 +1,12 @@
+package com.example.bookYourCab.model;
+
+import com.example.bookYourCab.enums.TripStatus;
+
+public class Booking {
+    private int bookingId;
+    private String pickUp;
+    private String destination;
+    private int distance;
+    private int bill;
+    private TripStatus tripStatus;
+}

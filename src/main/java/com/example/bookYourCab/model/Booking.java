cf1f2479647839bbd8af88b1a58f287a0b82cfd6
manjunathbhavi04 <manjunathbhavi890@gmail.com>
2025-01-14 16:56:23 +0530

    Annotations for cab and booking

--- src/main/java/com/example/bookYourCab/model/Booking.java
+++ src/main/java/com/example/bookYourCab/model/Booking.java
@@ -1,8 +1,20 @@
 package com.example.bookYourCab.model;
 
 import com.example.bookYourCab.enums.TripStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
 public class Booking {
+    @Id
     private int bookingId;
     private String pickUp;
     private String destination;
